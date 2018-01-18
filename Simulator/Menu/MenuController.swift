8d0c2d74192e6fe0d4fa50a99b7da0993a7bba80
kurokyfan <kuro2007cumt@126.com>
2018-01-18 20:30:48 +0800

    add Refresh button

--- Simulator/Menu/MenuController.swift
+++ Simulator/Menu/MenuController.swift
@@ -1,54 +1,71 @@
 import Cocoa
 
 class MenuController: NSObject, NSMenuDelegate {
-
-  let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
-  var devices: [Device] = []
-
-  override func awakeFromNib() {
-    super.awakeFromNib()
-
-    statusItem.image = NSImage(named: "icon")
-    statusItem.menu = makeMenu()
-  }
-
-  // MARK: - Menu
-
-  func makeMenu() -> NSMenu {
-    let menu = NSMenu()
-    menu.delegate = self
-    menu.autoenablesItems = false
-
-    devices = Device.load()
-    Menu.load(devices).forEach {
-      menu.addItem($0)
-    }
-
-    menu.addItem(NSMenuItem.separator())
-    menu.addItem(makeQuitItem())
-
-    return menu
-  }
-
-  func makeQuitItem() -> NSMenuItem {
-    let item = NSMenuItem()
-    item.title = "Quit"
-    item.isEnabled = true
-    item.target = self
-    item.action = #selector(quit(_:))
-
-    return item
-  }
-
-  // MARK: - Action
-
-  func quit(_ item: NSMenuItem) {
-    NSApplication.shared().terminate(self)
-  }
-
-  // MARK: - NSMenuDelegate
-
-  func menuWillOpen(_ menu: NSMenu) {
     
-  }
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    var devices: [Device] = []
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        statusItem.image = NSImage(named: "icon")
+        statusItem.menu = makeMenu()
+    }
+    
+    // MARK: - Menu
+    
+    func makeMenu() -> NSMenu {
+        let menu = NSMenu()
+        menu.delegate = self
+        menu.autoenablesItems = false
+        
+        devices = Device.load()
+        Menu.load(devices).forEach {
+            menu.addItem($0)
+        }
+        
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(makeRefreshItem())
+        menu.addItem(makeQuitItem())
+        
+        return menu
+    }
+    
+    func makeRefreshItem() -> NSMenuItem {
+        let item = NSMenuItem()
+        item.title = "Refresh"
+        item.isEnabled = true
+        item.target = self
+        item.keyEquivalent = "r"
+        item.action = #selector(refresh(_:))
+        
+        return item
+    }
+    
+    func makeQuitItem() -> NSMenuItem {
+        let item = NSMenuItem()
+        
+        item.title = "Quit"
+        item.isEnabled = true
+        item.target = self
+        item.keyEquivalent = "q"
+        item.action = #selector(quit(_:))
+        
+        return item
+    }
+    
+    // MARK: - Action
+    func refresh(_ item: NSMenuItem) {
+        statusItem.menu = makeMenu()
+    }
+    
+    func quit(_ item: NSMenuItem) {
+        NSApplication.shared().terminate(self)
+    }
+    
+    // MARK: - NSMenuDelegate
+    
+    func menuWillOpen(_ menu: NSMenu) {
+        
+    }
 }
